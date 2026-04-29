2c8b107d4d77c75923f1a772bcc92275af5b0223
Siddhant Desale <132735401+SiddhantDesale@users.noreply.github.com>
2026-04-29 15:01:48 +0530

    Delete src directory

--- src/constants/navConfig.js
+++
@@ -1,7 +0,0 @@
-export const NAV_ITEMS = [
-  { name: "Home", path: "/" },
-  { name: "Bollywood", path: "/language/bollywood" },
-  { name: "Hollywood", path: "/language/hollywood" },
-  { name: "Tollywood", path: "/language/tollywood" },
-  { name: "Web Series", path: "/webseries" },
-];
